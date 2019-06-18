0620fbe2f8f523e59b1e3c6434a06ce183b06041
Richie191 <richardmwilson191@gmail.com>
2019-06-18 10:35:01 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Getting Dom Elements
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+const copy = document.getElementById("copy");
+const year = new Date();
+
+//Set initial menu state
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set menu state
+    showMenu = false;
+  }
+}
+
+// Set copyright date
+copy.innerHTML = year.getFullYear();
